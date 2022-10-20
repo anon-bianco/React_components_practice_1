b7e67f4bf047d1b3496d24b75116c2fc26a46b69
anon-bianco <112154775+anon-bianco@users.noreply.github.com>
2022-10-20 11:13:08 +0530

    Initial commit

---
+++ src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Chicken Biriyani</li>
+      <li>Thandoori</li>
+      <li>Kalaki</li>
+    </ul>
+  );
+}
+
+export default List;
